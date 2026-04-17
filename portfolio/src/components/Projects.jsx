2c87cab61ba1759d6b4f1255d5f581cd490b18c8
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-18 05:25:50 +0530

    feat: addded images to project preview

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,23 +1,29 @@
 import { motion } from "framer-motion";
+import geminiImg from "../assets/gemini.png";
+import tacoImg from "../assets/taco.png";
+import notesImg from "../assets/notes.png";
 
 const projects = [
   {
     title: "Gemini Orbit",
     desc: "AI-powered knowledge graph generator using Gemini API.",
     tech: "React • Spring Boot • PostgreSQL",
-    link: "https://github.com/23-kartikey/gemini-orbit-ai-backend"
+    link: "https://github.com/23-kartikey/gemini-orbit-ai-backend",
+    image: tacoImg
   },
   {
     title: "Taco Cloud",
     desc: "Full-stack food ordering system with MVC architecture.",
     tech: "Spring Boot • MySQL",
-    link: "https://github.com/23-kartikey/taco-cloud"
+    link: "https://github.com/23-kartikey/taco-cloud",
+    image: geminiImg
   },
   {
     title: "Secure Notes API",
     desc: "JWT authentication system with Spring Security.",
     tech: "Spring Boot • JWT • MySQL",
-    link: "https://github.com/23-kartikey/secure-notes-api"
+    link: "https://github.com/23-kartikey/secure-notes-api",
+    image: notesImg
   }
 ];
 
@@ -49,16 +55,25 @@ export default function Projects() {
   whileHover={{ scale: 1.03 }}
   className="group relative bg-zinc-900 rounded-2xl overflow-hidden border border-zinc-800 transition duration-300 hover:-translate-y-1 hover:border-purple-500/40"
 >
-  {/* 🔥 Background hover gradient */}
+  {/*Background hover gradient */}
   <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition bg-gradient-to-br from-purple-500/10 to-blue-500/10"></div>
 
-  {/* ✅ WRAPPED CONTENT */}
+  {/* WRAPPED CONTENT */}
   <div className="relative z-10">
 
     {/* Preview */}
-    <div className="h-40 bg-gradient-to-br from-zinc-800 to-zinc-700 flex items-center justify-center text-gray-400">
-      Preview
-    </div>
+    <div className="h-40 overflow-hidden relative">
+
+  <img
+    src={p.image}
+    alt={p.title}
+    className="w-full h-full object-cover transition duration-500 group-hover:scale-105"
+  />
+
+  {/* overlay for readability */}
+  <div className="absolute inset-0 bg-black/20 group-hover:bg-black/10 transition"></div>
+
+</div>
 
     {/* Text */}
     <div className="p-6">
